65d698380d6bf62bd095ba60bd5d8b848a6db674
Shavunen <snhavunen@quinnipiac.edu>
2022-02-22 00:02:30 -0500

    added if statements

--- src/Vending.java
+++ src/Vending.java
@@ -13,27 +13,19 @@ public class Vending {
 
 		System.out.println(">Hello! Welcome to the vending machine:) \n>Please know that we only accept 1$ bills.");
 		System.out.println(">Please select a snack below!");
-
-<<<<<<< HEAD
 		
 		System.out.println("1: KitKat, price: 2.00 ");
 		System.out.println("2: Orange Juice, price: 3.00");
 		System.out.println("3: Cape Code Chips, price: 4.00");
 		System.out.println("4: Water, price: 1.00");
 		System.out.println("5: PopTarts, price: 2.00");
-=======
-		System.out.println("selection 1: KitKat, price: 2.00 ");
-		System.out.println("selection 2: Orange Juice, price: 3.00");
-		System.out.println("selection 3: Cape Code Chips, price: 4.00");
-		System.out.println("selection 4: Water, price: 1.00");
-		System.out.println("selection 5: PopTarts, price: 2.00");
->>>>>>> 8e03e41cda2876771f970682b547caabd47c4675
+
+
 
 		
 		double price = 0.00;
 				
 		int Selection = question.nextInt();
-<<<<<<< HEAD
 			if (Selection == 1) {
 				price = 2.00;
 			}else if (Selection == 2) {
@@ -49,48 +41,41 @@ public class Vending {
 			}
 		
 		System.out.println(">Please insert your money.");
+		System.out.println("Are you sure you would like to proceed with this transaction?"); 
 		
-		double money = question.nextInt();
+		String next = input.next(); 
 		
-		boolean enoughMoney = false;
-				
-		while (enoughMoney == false) {
-		 price = price - money;
-		  if (price == 0) {
-			  
-			  System.out.println("Thank you for the transaction");
-			  enoughMoney = true;
-	
-		  }else {
-			  System.out.println(">Please insert your money.");
-			  money = question.nextInt();
-			  
-		  }
-			
+		if (next.equalsIgnoreCase("Yes")) {
+		System.out.println("Please continue with your transaction"); 
+		
+		}
+		
+		else { 
+		System.out.println("No worries, here is a full refund."); 
+		System.exit (1); 
+		{
+
+	double money = question.nextInt();
+
+	boolean enoughMoney = false;
+
+	while(enoughMoney==false)
+	{
+		price = price - money;
+		if (price == 0) {
+
+			System.out.println("Thank you for the transaction, have a good day :)");
+			enoughMoney = true;
+
+		} else {
+			System.out.println(">Please insert your money.");
+			money = question.nextInt();
 
 		}
-		
-		return Selection;
-		}
-		
-		
+
 	}
-	
-=======
-		return Selection; }}
 
-		//int money = question.nextInt();
-		
-	
-		//while (snackMenu > money) {
-			//System.out.println("Please insert money"); 
-		//}
-		//return snackMenu-money;
-		//System.out.println("You picked a great choice, have a good day :)");
-	//}
+	return Selection;
 
-
-		
-//}}
->>>>>>> 8e03e41cda2876771f970682b547caabd47c4675
-
+}}
+		return Selection;}}
