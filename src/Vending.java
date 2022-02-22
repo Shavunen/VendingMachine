9d8c573cdb429114805c83061603f799c625c0fd
Dhruvi Patel <DhruviPatel@dhruvis-mbp.quinnipiac.edu>
2022-02-21 19:23:26 -0500

    test 4000

--- src/Vending.java
+++ src/Vending.java
@@ -1,33 +1,32 @@
-package src;
 
 import java.util.Scanner;
 
 public class Vending {
 
-	public static void main(String[] args) {
-		double Snacks = snackmenue();
+ public static void main(String[] args)	{
+		double Snacks = snackMenu();
 
-	}
-
-	private static double snackmenue() {
+ }
+	public static double snackMenu() {
 		Scanner question = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 
 		double Selection = 0.00;
 
+		System.out.println("Hello! Welcome to the vending machine:)");
 		System.out.println("Please select a snack from our vending machine!!");
-		
-		System.out.println("hey girl hey ");
 
+		
 		System.out.println("selection 1: KitKat, price 1.70");
 		System.out.println("selection 2: Orange Juice, price 2.50");
 		System.out.println("selection 3: Cape Code Chips, price 3.60");
 		System.out.println("selection 4: Water, price 1.40");
 		System.out.println("selection 5: PopTarts, price 2.10");
 
-		Selection = question.nextDouble();
+		Selection = question.nextInt();
 
 		return Selection;
 
 	}
-
+	
 }
