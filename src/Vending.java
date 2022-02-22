35443c6f4a88bdb7fe48c098e8e03854d750c9b1
Dhruvi Patel <DhruviPatel@dhruvis-mbp.quinnipiac.edu>
2022-02-21 22:16:21 -0500

    Money transaction

--- src/Vending.java
+++ src/Vending.java
@@ -4,31 +4,30 @@ import java.util.Scanner;
 public class Vending {
 
  public static void main(String[] args)	{
-		double Snacks = snackMenu();
-
+		double snacks = snackMenu();
  }
 	public static double snackMenu() {
 		Scanner question = new Scanner(System.in);
 		Scanner input = new Scanner(System.in);
 
-		double Selection = 0.00;
 
-		System.out.println("Hello! Welcome to the vending machine:)");
-		System.out.println("Please select a snack from our vending machine!!");
+		System.out.println(">Hello! Welcome to the vending machine:) \n>Please know that we only accept 1$ bills.");
+		System.out.println(">Please select a snack from our vending machine!!");
 
-		System.out.println("hi serena");
 		
-		
-		System.out.println("selection 1: KitKat, price 1.70");
-		System.out.println("selection 2: Orange Juice, price 2.50");
-		System.out.println("selection 3: Cape Code Chips, price 3.60");
-		System.out.println("selection 4: Water, price 1.40");
-		System.out.println("selection 5: PopTarts, price 2.10");
+		System.out.println("selection 1: KitKat, price: 2.00 ");
+		System.out.println("selection 2: Orange Juice, price: 3.00");
+		System.out.println("selection 3: Cape Code Chips, price: 4.00");
+		System.out.println("selection 4: Water, price: 1.00");
+		System.out.println("selection 5: PopTarts, price: 2.00");
 
-		Selection = question.nextInt();
+		int Selection = question.nextInt();
+		
+		System.out.println(">Please insert your money.");
+		
+		int money = question.nextInt();
 		
 		return Selection;
-
 	}
 	
 }
