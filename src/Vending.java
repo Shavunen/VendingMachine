cc98570b234a1d836518360ea3ef2584ada7ef6e
Dhruvi Patel <DhruviPatel@dhruvis-mbp.quinnipiac.edu>
2022-02-21 23:28:24 -0500

    wont let me pull

--- src/Vending.java
+++ src/Vending.java
@@ -15,19 +15,56 @@ public class Vending {
 		System.out.println(">Please select a snack from our vending machine!!");
 
 		
-		System.out.println("selection 1: KitKat, price: 2.00 ");
-		System.out.println("selection 2: Orange Juice, price: 3.00");
-		System.out.println("selection 3: Cape Code Chips, price: 4.00");
-		System.out.println("selection 4: Water, price: 1.00");
-		System.out.println("selection 5: PopTarts, price: 2.00");
+		System.out.println("1: KitKat, price: 2.00 ");
+		System.out.println("2: Orange Juice, price: 3.00");
+		System.out.println("3: Cape Code Chips, price: 4.00");
+		System.out.println("4: Water, price: 1.00");
+		System.out.println("5: PopTarts, price: 2.00");
 
+		
+		double price = 0.00;
+				
 		int Selection = question.nextInt();
+			if (Selection == 1) {
+				price = 2.00;
+			}else if (Selection == 2) {
+				price = 3.00;
+			}else if (Selection == 3) {
+				price = 4.00;
+			}else if (Selection == 4) {
+				price = 1.00;
+			}else if (Selection == 5) {
+				price = 2.00;
+			}else {
+				System.out.println("You did not enter a correct selection.");
+			}
 		
 		System.out.println(">Please insert your money.");
 		
-		int money = question.nextInt();
+		double money = question.nextInt();
+		
+		boolean enoughMoney = false;
+				
+		while (enoughMoney == false) {
+		 price = price - money;
+		  if (price == 0) {
+			  
+			  System.out.println("Thank you for the transaction");
+			  enoughMoney = true;
+	
+		  }else {
+			  System.out.println(">Please insert your money.");
+			  money = question.nextInt();
+			  
+		  }
+			
+
+		}
 		
 		return Selection;
+		}
+		
+		
 	}
 	
-}
+
